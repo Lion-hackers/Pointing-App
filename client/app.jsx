0d15be38e1e1195206404d519f916b1daeebd43b
ZitounBedis <bediszitoun1919@gmail.com>
2020-12-21 11:17:13 +0100

    done

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,8 @@
-import Contactuser from "./contactuser.js";
-import AboutUs from "./aboutus.js";
-
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      isHidden: "home",
-    };
+  
+    
   }
   switch(option) {
     this.setState({ isHidden: option });
@@ -14,26 +10,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {this.state.isHidden === "home" ? (
-          <div>
-            {" "}
-            <img id="logo" src="logo.png"></img>
-            <button
-              id="age"
-              onClick={() => {
-                this.switch("about");
-              }}
-            >
-              CONTINUE{" "}
-            </button>
-          </div>
-        ) : this.state.isHidden == "about" ? (
-          <AboutUs switch={this.switch.bind(this)} />
-        ) : (
-          <Contactuser />
-        )}
-
-        {/* <Contactuser/> */}
       </div>
     );
   }
