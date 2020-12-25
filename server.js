fe257c1a437519a3a78ef713415559868e0b40f2
ZitounBedis <bediszitoun1919@gmail.com>
2020-12-25 15:18:06 +0100

    step 3 done

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 1919;
+const mongoose = require("mongoose");
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded());
 app.get("/", (req, res) => {
     res.send("index.html");
 });
+mongoose.connect("mongodb://localhost:27017/pointingapp", {
+    useNewUrlParser: true,
+});
+var userSchema = new mongoose.Schema({
+    Username: {
+        type: String,
+    },
+});
+var User = mongoose.model("user", userSchema);
+app.post("/save", (req, res) => {
+    const obj = req.body;
+
+    console.log(obj);
+    User.create({
+        Username: req.body.Username,
+    });
+    res.redirect("./");
+});
+
 app.listen(port);
+// app.post("/save", (req, res) => {
+//     console.log("Employee Name : ",
+//         req.body.Username)
+//     res.redirect("./index.html");
+// });
