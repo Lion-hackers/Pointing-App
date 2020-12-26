c1f919e32fa82c68407cc5134fd4e435273abdb6
unknown <khaled.yeferni@gmail.com>
2020-12-26 14:56:59 +0100

    init Khaled branch

--- client/app.jsx
+++ client/app.jsx
@@ -1,4 +1,5 @@
  import Home from "./Home.js";
+ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
   constructor() {
     super(); 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 1919;
 app.use(express.static("public"));
 app.use(express.json());
 app.get("/", (req, res) => {
+    console.log("server running on port " + port)
     res.send("index.html");
 });
 app.listen(port);
